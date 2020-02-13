73fbcec5ab0c9bd0a0bbce12a003401a1926933a
Adrian Tineo <adrian@adriantineo.com>
2020-02-13 10:36:45 +0100

    Add UI for ResultsViewController

---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Adrian Tineo on 13.02.20.
+//  Copyright © 2020 Adrian Tineo. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBAction func didTapDone(_ sender: UIBarButtonItem) {
+        print("didTapDone")
+    }
+    
+    @IBOutlet weak var resultTextLabel: UILabel!
+    @IBOutlet weak var resultCommentLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
