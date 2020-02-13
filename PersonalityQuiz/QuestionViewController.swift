196cbbcb045f28b188df539dc68dc4cb52d978b6
Adrian Tineo <adrian@adriantineo.com>
2020-02-13 09:55:02 +0100

    Add subclasses of QuestionViewController

---
+++ PersonalityQuiz/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Adrian Tineo on 13.02.20.
+//  Copyright © 2020 Adrian Tineo. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
